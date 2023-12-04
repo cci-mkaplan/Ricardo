e7c795ac51308ad60bbb1b7cf3e9178bc630aa8c
mutlu kaplan <mutlukaplan@hotmail.com>
2023-12-04 19:33:47 +0000

    customer repos has been refactored

--- Ricardo.Technical.Test/Data/CustomerRepository.cs
+++ Ricardo.Technical.Test/Data/CustomerRepository.cs
@@ -1,49 +1,19 @@
 ﻿namespace Ricardo.Technical.Test.Data
 {
-	public class CustomerRepository
-	{
-		private readonly List<Customer> _customers = new();
+    public class CustomerRepository
+    {
+        private readonly List<Customer> _customers = new();
 
-		public CustomerRepository()
-		{
-			//Data seeding
-			_customers.Add(new Customer(BankAccount.Open(200), "test", "1234", "Jeremy Irons"));
-		}
+        public CustomerRepository()
+        {
+            //Data seeding
+            _customers.Add(new Customer(BankAccount.Open(200), "test", "1234", "Jeremy Irons"));
+        }
 
-		public Customer? FindCustomerByUsernameAndPassword(string username, string password)
-		{
-			var Customer = new Customer(null, "", "", "");
-			foreach (var customer in _customers)
-			{
-				bool found = false;
-				if (customer.Username == username)
-				{
-					if (customer.Password == password)
-					{
-						found = true;
-					}
-					else
-					{
-						found = false;
-					}
-				}
-
-				if (found == true)
-				{
-					Customer = customer;
-				}
-			}
-
-			if (Customer == null)
-			{
-				return new Customer(null, "", "", "");
-			}
-			else
-			{
-				return Customer;
-			}
-			return new Customer(null, "", "", "");
-		}
-
-	}
+        public Customer? FindCustomerByUsernameAndPassword(string username, string password)
+        {
+            var customer = _customers.Where(p => p.Username == username && p.Password == password).FirstOrDefault();
+            return customer;
+        }
+    }
 }
